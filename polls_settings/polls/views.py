f16a71ea2aa16465511cab0974d132d10faa15c3
Prakhar <44711005+Prag066@users.noreply.github.com>
2019-07-18 18:02:08 +0530

    Add files via upload

---
+++ polls_settings/polls/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Question,Choice
+from django.template import loader
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[0:2]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list':latest_question_list
+    }
+    return render(request,'polls/index.html',context)
+    #return HttpResponse(template.render(context,request))
+
+
+def detail(request,question_id):
+    # try:
+    #     question = Question.objects.get(id=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/detail.html',{"question":question})
+
+def results(request,question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response %question_id)
+
+
+def vote(request,question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
