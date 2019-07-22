d074a4c999fa7b2d689ffa803b87b95e21db8284
Prakhar <44711005+Prag066@users.noreply.github.com>
2019-07-22 13:53:59 +0530

    Add files via upload

--- polls_settings/polls/models.py
+++ polls_settings/polls/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    id = models.AutoField
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
