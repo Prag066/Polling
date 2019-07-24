67c74cd1431a8958b9738f7cae3490cd2f1a29a9
Prakhar <44711005+Prag066@users.noreply.github.com>
2019-07-24 11:47:27 +0530

    Add files via upload

--- polls_settings/polls/views.py
+++ polls_settings/polls/views.py
@@ -4,6 +4,7 @@ from .models import Question,Choice
 from django.template import loader
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
+from django.utils import timezone
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[0:2]
@@ -21,11 +22,22 @@ class IndexView(ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+
+        """Return the last five published questions (not including those set to be
+    published in the future)."""
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 class DetailView(DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(DetailView):
